b3dde7162ee739b83b1fc70e36754c2279ddeea7
KiranWells <kiranwells1008@gmail.com>
2023-11-18 18:16:37 -0600

    Add partial theme and styling

--- blackbox/src/ui/mod.rs
+++ blackbox/src/ui/mod.rs
@@ -2,9 +2,9 @@ use std::sync::Arc;
 
 use color_eyre::Result;
 use iced::{
-    theme::{Button, Container},
+    theme::Button,
     widget::{button, column, container, row, scrollable, text, Row, Space},
-    Application, Color, Command, Executor, Length, Settings, Theme,
+    Application, Command, Executor, Font, Length, Settings,
 };
 use tokio::sync::{Mutex, Notify};
 
@@ -12,6 +12,10 @@ use crate::types::{
     AccessType, Connection, FileAccess, FileSummary, NetworkSummary, ProcessSummary,
     ProcessingData, SpawnEvent,
 };
+
+mod theme;
+
+use theme::{ContainerType, Theme};
 
 struct CurrentExecutor {}
 
@@ -62,7 +66,7 @@ struct Flags {
     shared_state: Arc<Mutex<Option<ProcessingData>>>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Tab {
     Summary,
     File,
@@ -120,14 +124,26 @@ impl Application for App {
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        let tabs = column!(
-            button("Summary").on_press(Message::SwitchTab(Tab::Summary)),
-            button("Files").on_press(Message::SwitchTab(Tab::File)),
-            button("Network").on_press(Message::SwitchTab(Tab::Network)),
-            button("Processes").on_press(Message::SwitchTab(Tab::Process)),
-        );
-        // TODO: add the actual information in here
-        let main_view: iced::Element<_> = match &self.state {
+        let tabs = column(
+            vec![Tab::Summary, Tab::File, Tab::Network, Tab::Process]
+                .into_iter()
+                .map(|tab| {
+                    button(text(format!("{:?}", tab)))
+                        .on_press(Message::SwitchTab(tab))
+                        .style(if matches!(self.state, State::OnTab(t) if t == tab) {
+                            Button::Primary
+                        } else {
+                            Button::Secondary
+                        })
+                        .width(Length::Fill)
+                        .into()
+                })
+                .collect(),
+        )
+        .spacing(10)
+        .padding(10)
+        .width(Length::FillPortion(1));
+        match &self.state {
             State::Processing => row!(
                 Space::new(Length::FillPortion(3), 0),
                 text("Processing, please wait. If you need to interact with the process, use the command line where Blackbox was started.").size(20).width(Length::FillPortion(3)),
@@ -135,20 +151,14 @@ impl Application for App {
             ).into(),
             State::OnTab(tab) => row!(
                 tabs,
-                match tab {
+                container(match tab {
                     Tab::Summary => Self::summary_view(&self.data),
                     Tab::File => Self::file_view(&self.data),
                     Tab::Network => Self::network_view(&self.data),
                     Tab::Process => Self::process_view(&self.data),
-                },
+                }).width(Length::FillPortion(3)),
             ).into()
-        };
-        container(main_view)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_x()
-            .center_y()
-            .into()
+        }
     }
 }
 
@@ -164,32 +174,34 @@ impl App {
             .alerts
             .iter()
             .map(|alert| {
-                button(text(&alert.message))
+                container(text(&alert.message))
                     .style(match alert.severity {
-                        0 => Button::Destructive,
-                        1 => Button::Secondary,
-                        2 => Button::Positive,
-                        _ => Button::Primary,
+                        0 => ContainerType::Red,
+                        1 => ContainerType::Orange,
+                        2 => ContainerType::Yellow,
+                        _ => ContainerType::Card,
                     })
+                    .padding(5)
+                    .width(Length::Fill)
                     .into()
             })
-            .collect::<Vec<iced::Element<_>>>();
-        let alerts = scrollable(column(alerts)).height(100).width(Length::Fill);
+            .collect::<Vec<Element>>();
+        let alerts = scrollable(column(alerts).spacing(5))
+            .height(100)
+            .width(Length::Fill);
 
         let file_summary = row![
-            container(text("File access:"))
-                .padding(10)
-                .width(Length::FillPortion(1)),
+            container(text("File access:")).padding(10).width(100),
             Self::create_file_summary(&data.file_summary),
         ];
 
         let network_summary = row![
-            container(text("Network:")).padding(10),
+            container(text("Network:")).padding(10).width(100),
             Self::create_network_summary(&data.network_summary)
         ];
 
         let process_summary = row![
-            container(text("Processes:")).padding(10),
+            container(text("Processes:")).padding(10).width(100),
             Self::create_process_summary(&data.process_summary)
         ];
 
@@ -199,7 +211,8 @@ impl App {
             file_summary,
             network_summary,
             process_summary
-        );
+        )
+        .spacing(10);
         scrollable(main_view)
             .width(Length::Fill)
             .height(Length::Fill)
@@ -255,22 +268,28 @@ impl App {
         let mut boxes = vec![];
         if access.read {
             boxes.push(
-                container(text("R").style(Color::from_rgba(0.9, 0.1, 0.0, 0.7)))
-                    .padding(5)
+                container(text("R"))
+                    .style(ContainerType::Green)
+                    .width(25)
+                    .height(25)
                     .into(),
             );
         }
         if access.write {
             boxes.push(
-                container(text("W").style(Color::from_rgba(0.1, 0.9, 0.0, 0.7)))
-                    .padding(5)
+                container(text("W"))
+                    .style(ContainerType::Yellow)
+                    .width(25)
+                    .height(25)
                     .into(),
             );
         }
         if access.execute {
             boxes.push(
-                container(text("X").style(Color::from_rgba(0.1, 0.1, 0.9, 0.7)))
-                    .padding(5)
+                container(text("X"))
+                    .style(ContainerType::Red)
+                    .width(25)
+                    .height(25)
                     .into(),
             );
         }
@@ -309,7 +328,7 @@ impl App {
                 ])
                 .padding(10)
                 .width(Length::FillPortion(1))
-                .style(Container::Box),
+                .style(ContainerType::Card),
                 container(column![
                     text(format!("Files Accessed: {}", file_summary.access_count)),
                     text(format!("Total Bytes Read: {}", file_summary.bytes_read)),
@@ -319,13 +338,13 @@ impl App {
                     )),
                 ])
                 .width(Length::FillPortion(1))
-                .style(Container::Box),
+                .style(ContainerType::Card),
             ],
             column(
                 file_summary
                     .directories
                     .iter()
-                    .map(|dir| text(dir.to_string_lossy()).into())
+                    .map(|dir| monospace(dir.to_string_lossy()))
                     .collect()
             ),
         ]
@@ -377,13 +396,16 @@ impl App {
             )),
             row![
                 text("Processes executed:"),
-                scrollable(column(
-                    process_summary
-                        .programs
-                        .iter()
-                        .map(|program| text(program.to_string_lossy()).into())
-                        .collect()
-                )),
+                scrollable(
+                    column(
+                        process_summary
+                            .programs
+                            .iter()
+                            .map(|program| monospace(program.to_string_lossy()))
+                            .collect()
+                    )
+                    .spacing(5)
+                ),
             ],
         ]
         .into()
@@ -395,17 +417,19 @@ impl App {
             .push(Self::create_hex_preview(&access.write_data));
         column![
             row![
-                container(text(access.file_descriptor)),
+                monospace(access.file_descriptor),
                 text(
                     access
                         .file_name
                         .clone()
                         .map(|s| s.to_string_lossy().to_string())
                         .unwrap_or("No file name".into())
-                ),
+                )
+                .font(Font::MONOSPACE)
+                .size(20),
                 Self::create_access_mark(access.access_type),
             ],
-            data_views,
+            data_views.width(Length::Fill),
         ]
         .into()
     }
@@ -417,17 +441,19 @@ impl App {
                 .map(|chunk| {
                     row(chunk
                         .iter()
-                        .map(|b| container(text(format!("{:x}", b))).into())
+                        .map(|b| monospace(format!("{:02x}", b)))
                         .collect())
+                    .spacing(3)
                     .into()
                 })
                 .collect(),
-        );
+        )
+        .spacing(3);
         let string = column(
             bytes
                 .chunks_exact(16)
                 .map(|chunk| {
-                    text(
+                    monospace(
                         chunk
                             .iter()
                             .map(|b| {
@@ -440,11 +466,11 @@ impl App {
                             })
                             .collect::<String>(),
                     )
-                    .into()
                 })
                 .collect(),
-        );
-        row!(hex, string).into()
+        )
+        .spacing(3);
+        row!(hex, string).spacing(10).into()
     }
 
     fn create_connection(conn: &Connection) -> Element<'static> {
@@ -459,18 +485,25 @@ impl App {
     fn create_spawn_event(spawn: &SpawnEvent) -> Element<'static> {
         column![
             row![
-                text(
+                monospace(
                     spawn
                         .command
                         .clone()
                         .unwrap_or("No Command".into())
                         .to_string_lossy()
                 ),
-                text(format!("{} -> {}", spawn.parent_id, spawn.process_id))
+                monospace(format!("{} -> {}", spawn.parent_id, spawn.process_id))
             ],
             text(format!("Timestamp: {}", spawn.spawn_time)),
             text(format!("Spawn Type: {:?}", spawn.spawn_type)),
         ]
         .into()
     }
+}
+
+fn monospace(t: impl ToString) -> Element<'static> {
+    container(text(t).font(Font::MONOSPACE))
+        .style(ContainerType::Card)
+        .padding(3)
+        .into()
 }
